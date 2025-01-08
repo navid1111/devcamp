272f8fcc6615c7dd0df320c95faf6a467f20d5e7
navid-kamal <navidkamal@iut-dhaka.edu>
2025-01-08 20:33:57 +0600

    implemented deletion of a bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -35,6 +35,9 @@ exports.getBootcamps= async(req,res,next)=>{
 
     
 }
+// @desc   Get single bootcamp
+// @route  GET /api/v1/bootcamp
+// @access Public
 exports.getBootcamp=async(req,res,next)=>{
     try {
         const bootcamp= await Bootcamp.findById(req.params.id);
@@ -49,4 +52,43 @@ exports.getBootcamp=async(req,res,next)=>{
         res.status(400).json({success:false})
         
     }
+}
+// @desc   Update a bootcamp
+// @route  PUT /api/v1/bootcamp
+// @access Private
+
+exports.updateBootcamp=async(req,res,next)=>{
+    try {
+        const bootcamp=await Bootcamp.findByIdAndUpdate(req.params.id,req.body,{
+            new:true,
+            runValidators:true
+        })
+        if(!bootcamp){
+            return res.status(400).json({success:false})
+        }
+        res.status(200).json({success:true,data:bootcamp})
+        
+    } catch (error) {
+        return res.status(400).json({success:false})
+        
+    }
+ 
+}
+// @desc   Delete a bootcamp
+// @route   /api/v1/bootcamp
+// @access Private
+
+exports.deleteBootcamp=async(req,res,next)=>{
+    try {
+        const bootcamp=await Bootcamp.findByIdAndDelete(req.params.id)
+        if(!bootcamp){
+            return res.status(400).json({success:false})
+        }
+        res.status(200).json({success:true,data:{}})
+        
+    } catch (error) {
+        return res.status(400).json({success:false})
+        
+    }
+ 
 }
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,7 +1,9 @@
-const {getBootcamps, createBootcamp, getBootcamp} = require('../controllers/bootcamps.js')
+const {getBootcamps, createBootcamp, getBootcamp, updateBootcamp, deleteBootcamp} = require('../controllers/bootcamps.js')
 const express=require('express')
 const router=express.Router();
 router.get('/',getBootcamps)
 router.post('/',createBootcamp)
 router.get('/:id',getBootcamp)
+router.put('/:id',updateBootcamp)
+router.delete('/:id',deleteBootcamp)
 module.exports=router
